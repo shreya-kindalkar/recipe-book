0e2f2019600c52e70bab89b82e5d9e400c043f72
shreya-kindalkar <shreyakindalkar7@gmail.com>
2025-11-21 13:12:07 +0530

    Fix: Correct login url routing

--- core/urls.py
+++ core/urls.py
@@ -1,4 +1,4 @@
 from django.urls import path
 from django.shortcuts import render
 urlpatterns = [ path ('signup/', lambda request: render(request,'core/signup.html',{'title':'Sign Up'})),
-             path ('login/',lambda request:render(request,'core/signup.html',{'title':'Login'}))]
+             path ('login/',lambda request:render(request,'core/login.html',{'title':'Login'}))]
