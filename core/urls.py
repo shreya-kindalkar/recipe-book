71baa22b1bc7621677d99289e714045b71d9398f
shreya-kindalkar <shreyakindalkar7@gmail.com>
2025-11-23 22:22:43 +0530

    Add: URL routing for Sign Up page

--- core/urls.py
+++ core/urls.py
@@ -1,4 +1,3 @@
 from django.urls import path
-from django.shortcuts import render
-urlpatterns = [ path ('signup/', lambda request: render(request,'core/signup.html',{'title':'Sign Up'})),
-             path ('login/',lambda request:render(request,'core/login.html',{'title':'Login'}))]
+from . import views
+urlpatterns = [ path ('signup/', views.signup, name='signup')]
