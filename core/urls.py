f4ad8a9dd0ab0e265af021e1f8427fc220196646
shreya-kindalkar <shreyakindalkar7@gmail.com>
2025-11-25 20:56:34 +0530

    Added: URL routing for dashboard and logout

--- core/urls.py
+++ core/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path
 from . import views
 urlpatterns = [ path ('signup/', views.signup, name='signup'),
-               path ('login/',views.user_login, name='login')]
+               path ('login/',views.user_login, name='login'),
+               path('dashboard/',views.dashboard, name='dashboard'),
+               path('logout/',views.logout,name='logout')]
