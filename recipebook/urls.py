f24eca6b675bad306ff67f70de2552aa5eff8a95
shreya-kindalkar <shreyakindalkar7@gmail.com>
2025-11-26 10:15:56 +0530

    Added: URL routing for home page

--- recipebook/urls.py
+++ recipebook/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from core import views
 from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('core.urls'))
+    path('',include('core.urls')),
+    path('',views.home,name='home')
 ]
