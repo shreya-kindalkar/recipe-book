4bbd43cc535e3e698bd57b2f33719586a4349dc9
shreya-kindalkar <shreyakindalkar7@gmail.com>
2025-11-26 17:07:07 +0530

    Fix: add recipe view name

--- core/urls.py
+++ core/urls.py
@@ -3,4 +3,6 @@ from . import views
 urlpatterns = [ path ('signup/', views.signup, name='signup'),
                path ('login/',views.user_login, name='login'),
                path('dashboard/',views.dashboard, name='dashboard'),
-               path('logout/',views.logout,name='logout')]
+               path('logout/',views.logout,name='logout'),
+               path('add-recipe/', views.add_recipe, name='add_recipe'),
+]
