ed65fa847a4b4fa37f92e4f9f5005c3cd11c1407
shreya-kindalkar <shreyakindalkar7@gmail.com>
2025-11-27 16:02:21 +0530

    Fix: URL routing by moving home route to core.urls

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
-urlpatterns = [ path ('signup/', views.signup, name='signup'),
+urlpatterns = [path('', views.home, name='home'),
+               path ('signup/', views.signup, name='signup'),
                path ('login/',views.user_login, name='login'),
                path('dashboard/',views.dashboard, name='dashboard'),
                path('logout/',views.user_logout,name='logout'),
