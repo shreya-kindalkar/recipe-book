2df4f1d78dc741d86a1a4a0888c7f5860d112e74
shreya-kindalkar <shreyakindalkar7@gmail.com>
2025-11-27 13:17:31 +0530

    Add: Recipe models

--- core/models.py
+++ core/models.py
@@ -1,3 +1,13 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+class Recipe(models.Model):
+    user=models.ForeignKey(User,on_delete=models.CASCADE)
+    title=models.CharField(max_length=200)
+    ingrediets=models.TextField()
+    instructions=models.TextField()
+    image=models.ImageField(upload_to='recipes/',blank=True,null=True)
+    created_at=models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
