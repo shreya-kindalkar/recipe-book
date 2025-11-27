72a6e6f5d1dc66b9d40ac7c431113357f25d9aa7
shreya-kindalkar <shreyakindalkar7@gmail.com>
2025-11-27 16:00:04 +0530

    Add media URL patterns for serving uploaded recipe images

--- recipebook/urls.py
+++ recipebook/urls.py
@@ -17,9 +17,12 @@ Including another URLconf
 from django.contrib import admin
 from core import views
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('core.urls')),
-    path('',views.home,name='home')
 ]
+if  settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
