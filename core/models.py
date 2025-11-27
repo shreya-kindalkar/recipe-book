cab1b32149059d437da626a7c6b907fab19ae7bb
shreya-kindalkar <shreyakindalkar7@gmail.com>
2025-11-27 16:25:31 +0530

    Fix: type error in ingredients

--- core/models.py
+++ core/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 class Recipe(models.Model):
     user=models.ForeignKey(User,on_delete=models.CASCADE)
     title=models.CharField(max_length=200)
-    ingrediets=models.TextField()
+    ingredients=models.TextField()
     instructions=models.TextField()
     image=models.ImageField(upload_to='recipes/',blank=True,null=True)
     created_at=models.DateTimeField(auto_now_add=True)
