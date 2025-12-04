7fbe64d4162a40fdf1d096c6095d4c1919cdd834
gourijain026@gmail.com <gourijain@example.com>
2025-12-04 12:05:32 +0530

    Updated profile system

--- core/urls.py
+++ core/urls.py
@@ -1,5 +1,8 @@
+
+
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('', views.home, name='home'),
@@ -7,9 +10,19 @@ urlpatterns = [
     path('login/', views.user_login, name='login'),
     path('dashboard/', views.dashboard, name='dashboard'),
     path('logout/', views.user_logout, name='logout'),
+
     path('add-recipe/', views.add_recipe, name='add_recipe'),
-    path('add-profile/', views.add_profile, name='add_profile'), 
     path("recipe/<int:id>/", views.recipe_detail, name="recipe_detail"),
     path("recipe/<int:id>/delete/", views.delete_recipe, name="delete_recipe"),
-]
 
+
+    path('add-profile/', views.add_profile, name='add_profile'),
+
+
+    path('password-change/', 
+         auth_views.PasswordChangeView.as_view(
+             template_name='password_change.html',
+             success_url='/'
+         ),
+         name='password_change'),
+]
